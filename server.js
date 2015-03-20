feb2ffe86c55c475a2d210bfd559ea044146c17a
SeeNotEvil <evil922222@mail.ru>
2015-03-20 21:14:23 +1100

    #1

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 
-	var express = require('express') ;
-	var app = express();
-	
 
-	var port = process.env.PORT || 5000;       
-	app.listen(port)                           // Запускаем сервер на 5000 порту, если не указана переменная окружения "port" 
-	console.log("Listening at " + port)
 	
 	
 	
