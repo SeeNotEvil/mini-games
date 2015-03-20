c52e88f0d46c049d8057e52498500683a89be036
SeeNotEvil <evil922222@mail.ru>
2015-03-20 21:24:22 +1100

    #1

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
+	console.log("Listening at " + port) ;
+	
+	var express = require('express') ;
+	var app = express();
+	
 
+	var http = require('http').Server(app);
+	//var io = require('socket.io')(http);
+	http.listen(3000);
+	
 
+	//app.use(express.static(__dirname + '/public'));
+	
+	//Создаем и инициализируем экземпляр платформы
+ 	//var application = require('./application') ;
+	//var platform = new application() ;
+   // platform.init() ;
+
+	
+	
+	//Присоеденяемся к игре
+	/*io.sockets.on('connection', function(socket) 
+	{
+		//Добавляем клиента
+		platform.onSocketHandlers(socket) ;
+	});*/
 	
 	
 	
