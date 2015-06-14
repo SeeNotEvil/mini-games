fbfc85770f76ac156b9b3670e3f4249ef7775c5a
SeeNotEvil <evil922222@mail.ru>
2015-06-14 15:32:05 +1100

    1

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-	var express = require('express') ;
+	/*var express = require('express') ;
 	var app = express();
 
     //var fs = require('fs');
@@ -57,4 +57,4 @@
 
 
 
-    app.use(express.static(__dirname));
+   // app.use(express.static(__dirname));
